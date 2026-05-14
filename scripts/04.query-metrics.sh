69bad9f73163a7237ea79bf517ddd50fedc6a107
carmelomartin-spec <carmelo.martin@fluxaria.com>
2026-05-14 10:09:15 +0200

    Metrics script added

---
+++ scripts/04.query-metrics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Se puede hacer un grep por "gateway_proxy" o por "clientes-consulta-v1" o por "camel_" para mayor claridad 
+
+curl -sS http://localhost:8081/actuator/prometheus
+
+exit 0;
+
